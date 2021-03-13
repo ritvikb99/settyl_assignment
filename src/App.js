8f823c3597efef06dfe33a78f7701b0bc73c2ef9
ritvikb99 <ritvikb99@gmail.com>
2021-03-13 18:11:41 +0530

    Introduced hooks

--- src/App.js
+++ src/App.js
@@ -1,46 +1,55 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import CardsList from './CardsList';
 import SearchBox from './SearchBox';
 //import {robots} from './robots';
 import './App.css';
 import Scroll from './Scroll';
 
-class App extends React.Component{
-    constructor(props){
+const App = () => {
+  /* constructor(props){
         super(props);         //Always pass props to base constructor
         this.state = {
             robots: [],
             searchField : "",
         };
-    }
+    } */
 
-    componentDidMount(){
+  /* componentDidMount(){
         fetch('https://jsonplaceholder.typicode.com/users').then(response=>response.json())
         .then(users=>this.setState({robots: users}));
-    }
-    onSearchChange = (event)=>{
-        this.setState({
-            searchField: event.target.value,
-        });
-    }
-    render() {
-        let filteredRobots = this.state.robots.filter(robot=>{
-            return(robot.name.toLowerCase().includes(this.state.searchField.toLowerCase()));
-        });        
-        return(
-            <Fragment>
-                <div className="tc">
-                <h1 className="f1">ReactSearch</h1>
-                <h1 className="f1">With JSON placeholder</h1>
-                <div><SearchBox searchChange={this.onSearchChange}/></div>
-                <br></br>
-                <Scroll>
-                    <CardsList robots = {filteredRobots} />
-                </Scroll>
-                </div>
-            </Fragment>
-        );
-    }
-}
+    } */
+
+  const [robots, setRobots] = useState([]);
+  const [searchField, setSearchField] = useState('');
+
+  const onSearchChange = (event) => {
+    setSearchField(event.target.value);
+  };
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => setRobots(users));
+    console.log('hello');
+  }, []);
+  let filteredRobots = robots.filter((robot) => {
+    return robot.name.toLowerCase().includes(searchField.toLowerCase());
+  });
+
+  return (
+    <Fragment>
+      <div className='tc'>
+        <h1 className='f1'>ReactSearch</h1>
+        <h1 className='f1'>With JSON placeholder</h1>
+        <div>
+          <SearchBox searchChange={onSearchChange} />
+        </div>
+        <br></br>
+        <Scroll>
+          <CardsList robots={filteredRobots} />
+        </Scroll>
+      </div>
+    </Fragment>
+  );
+};
 
 export default App;
